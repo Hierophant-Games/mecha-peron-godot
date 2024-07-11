e1762807a0917dc7dde4b44c2b40cc7eeff9335c
Santiago M. Vilar <smvilar@gmail.com>
2024-07-11 00:56:00 -0300

    Fix EnemyProjectile movement to use gravity acceleration

--- game/EnemyProjectile.gd
+++ game/EnemyProjectile.gd
@@ -1,15 +1,18 @@
 class_name EnemyProjectile
 extends Destroyable
 
-@export var velocity: Vector2 = Vector2()
+@export var velocity: Vector2
+@export var gravity: float = Constants.GRAVITY
 
 func _ready():
-	area_2d.area_entered.connect(self._on_area_entered)
+	area_2d.area_entered.connect(_on_area_entered)
 
 func _process(delta):
-	if owner.get_meta("destroyed", false):
+	if owner.get_meta("destroyed"):
 		return
-	owner.position += Vector2(velocity.x * delta, (velocity.y + Constants.GRAVITY) * delta)
+	
+	velocity.y += gravity * delta
+	owner.position += velocity * delta
 
 func _enter_tree():
 	assert(owner is Node2D, "Owner must be Node2D")
