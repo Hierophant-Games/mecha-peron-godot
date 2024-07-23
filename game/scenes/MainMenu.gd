608bc2302147e24481156949f81b38153678b9fa
Santiago M. Vilar <smvilar@gmail.com>
2024-07-23 12:00:13 -0300

    Add Help screen

--- game/scenes/MainMenu.gd
+++ game/scenes/MainMenu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export_file("*.tscn") var play_scene: String
 @export_file("*.tscn") var credits_scene: String
+@export_file("*.tscn") var help_scene: String
 
 func _on_play_button_pressed():
 	$SceneFader.transition_to(play_scene)
@@ -13,4 +14,4 @@ func _on_credits_button_pressed():
 	$SceneFader.transition_to(credits_scene, false)
 
 func _on_help_button_pressed():
-	print("Help not implemented yet")
+	$SceneFader.transition_to(help_scene, false)
