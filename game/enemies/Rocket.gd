d008bf086b91e497b8e8ea28225265a78a606733
Santiago M. Vilar <smvilar@gmail.com>
2024-07-25 17:17:43 -0300

    Simplified some numbers in the health bar and collision positioning

--- game/enemies/Rocket.gd
+++ game/enemies/Rocket.gd
@@ -5,4 +5,3 @@ func _ready():
 	var projectile = $EnemyProjectile as EnemyProjectile
 	projectile.damage = Constants.SOLDIER_BULLET_DAMAGE
 	projectile.velocity = Vector2(Constants.SOLDIER_BULLET_SPEED, (randf() * 10)-5)
-	projectile.gravity = 0
