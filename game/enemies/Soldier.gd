3f87f33a0abef064c153febb1c9017e8110a87be
Nicolas Viegas Palermo <viegaspalermo@gmail.com>
2024-08-22 01:37:43 -0300

    Soldiers Sound Pass
    Preloads shoot, reload and death audio files on enemy building and sets up all soldiers to access them
    Plays a random sample of each on shooting, reloading and dying respectively

--- game/enemies/Soldier.gd
+++ game/enemies/Soldier.gd
@@ -6,10 +6,16 @@ const RocketScene := preload("res://game/enemies/Rocket.tscn")
 @onready var width: int = $Sprite2D.texture.get_width() / $Sprite2D.hframes
 @onready var health_bar := $HealthBar as HealthBar
 @onready var main_layer := get_tree().current_scene.main_layer as Node
+@onready var sfx_voice := $Voice as AudioStreamPlayer
+
+var sfx_shoot: Array[AudioStream]
+var sfx_reload: Array[AudioStream]
+var sfx_death: Array[AudioStream]
 
 var shoot_timer := 0.0
 
 func _ready():
+	sfx_voice.volume_db = linear_to_db(Constants.SOLDIER_VOLUME_MODIFIER)
 	$Sprite2D.frame = 0
 	shoot_timer = randf() * Constants.SOLDIER_SHOOT_TIME #randomize start time for shooting
 
@@ -20,6 +26,11 @@ func _process(delta: float):
 	if $Sprite2D.is_visible_in_tree():
 		aim(delta)
 
+func setup(preloaded_sfx_shoot, preloaded_sfx_reload, preloaded_sfx_death):
+	sfx_shoot = preloaded_sfx_shoot
+	sfx_reload = preloaded_sfx_reload
+	sfx_death = preloaded_sfx_death
+
 func aim(delta: float):
 	shoot_timer += delta
 	if shoot_timer >= Constants.SOLDIER_SHOOT_TIME:
@@ -28,9 +39,13 @@ func aim(delta: float):
 	
 func shoot():
 	$AnimationPlayer.play("shoot")
+	sfx_voice.set_stream(sfx_shoot.pick_random())
+	sfx_voice.play()
 	
 func reload():
 	$AnimationPlayer.play("reload")
+	sfx_voice.set_stream(sfx_reload.pick_random())
+	sfx_voice.play()
 
 func spawn_rocket():
 	var rocket := RocketScene.instantiate() as Rocket
@@ -41,6 +56,8 @@ func on_health_depleted():
 	die()
 
 func die():
+	sfx_voice.set_stream(sfx_death.pick_random())
+	sfx_voice.play()
 	if destroyed:
 		return
 	$Area2D.queue_free()
