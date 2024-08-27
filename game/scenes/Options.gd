b5908bd299d6a0bade7087ed344a2e962f25b60c
Nicolas Viegas Palermo <viegaspalermo@gmail.com>
2024-08-27 00:38:23 -0300

    Sets up sound buses, regulates volume according to settings
    Adds voice volume setting

--- game/scenes/Options.gd
+++ game/scenes/Options.gd
@@ -4,10 +4,12 @@ extends Control
 
 @onready var music_slider := $MarginContainer/VBoxContainer/MusicSettings/Slider as HSlider
 @onready var sound_slider := $MarginContainer/VBoxContainer/SoundSettings/Slider as HSlider
+@onready var voice_slider := $MarginContainer/VBoxContainer/VoiceSettings/Slider as HSlider
 
 func _ready():
 	music_slider.value = GameSettings.music_volume
 	sound_slider.value = GameSettings.sound_volume
+	voice_slider.value = GameSettings.voice_volume
 
 func _on_back_button_pressed():
 	$SceneFader.transition_to(back_scene, false)
@@ -17,3 +19,6 @@ func _on_music_slider_value_changed(value):
 
 func _on_sound_slider_value_changed(value):
 	GameSettings.sound_volume = value
+	
+func _on_voice_slider_value_changed(value):
+	GameSettings.voice_volume = value
